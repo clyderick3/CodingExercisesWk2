bc422c3487100fa6f4618c6efdf36ffbdd767c05
Clyde Broderick <clyderick3@gmail.com>
2022-03-05 01:35:23 -0500

    Copied over notes.

--- solution.java
+++ solution.java
@@ -2,91 +2,96 @@ public class Solution {
   
   // use this method as a prototype for your answers...
   void soln1() {
-  // and put your code here.
+  // For loop from 100 to 5 by 5. print index var.
   }
   
   void soln2() {
-  // and put your code here.
+  // For loop from 100 to 5 by 5. do nothing inside the loop.
   }
   
   void soln3() {
-  // and put your code here.
+  // Sum Integers from 9 to 17, print sum
   }
   
   void soln4() {
-  // and put your code here.
+  // Find and print the smallest integer in an array a = [9, 5, 6, 3, 8, 2, 4];
   }
   
   void soln5() {
-  // and put your code here.
+  // Find largest integer in the last array and print it.
   }
   
   void soln6() {
-  // and put your code here.
+  // Find and print index of first true element in an array of booleans a = [false, false, false, true, false];
   }
   
   void soln7() {
-  // and put your code here.
+  // Find and print index of first true element in an array of booleans (must use a while loop)
   }
   
   void soln8() {
-  // and put your code here.
+  // add 5 to every element of an array of integers a = [9, 5, 6, 3, 8, 2, 4]
   }
   
   void soln9() {
-  // and put your code here.
+  // divide each element of an array by 1.3; a = [0.5, 1.4, 6.7, 123.4 -34.6]
   }
   
   void soln10() {
-  // and put your code here.
+  // raise each element of a double array to it's 3rd power and subtract the original element's value by the value divided by 3.0 a = [1.3, 5.4, 6.1, 1.0, 9.2];
   }
   
   void soln11() {
-  // and put your code here.
+  // zero out a 4 by 6 integer array
   }
   
   void soln12() {
-  // and put your code here.
+  // multiply each element of a 5 by 5 array of integers by 7
   }
   
   void soln13() {
-  // and put your code here.
+  // create a method that takes a 5 by 5 array and produces a 5 by 5 array of booleans. each element in the result
+    // should be true if the value in the argument array is even, else it's false.
   }
   
   void soln14() {
-  // and put your code here.
+  // create a method that takes two doubles and returns the first parameter raised to the second parameter's value
   }
   
   void soln15() {
-  // and put your code here.
+  // create a method that returns a boolean if the Object passed to it is a Fish or not
   }
   
   void soln16() {
-  // and put your code here.
+  // There is an object x which has a method (double)Balance(). Build an IF statement that prints
+    // "Paid Off" if the result of the balance call is less than of equal to 1.0, else it prints
+    // the balance as a Dollar figure (2 decimal places)
   }
   
   void soln17() {
-  // and put your code here.
+  // Build a WHILE loop that turns on the heat if the thermo.getTemp()
+    // is less than 72.0 else it turns on the A/C if thermo.getTemp() is greater that 76.0.
   }
   
   void soln18() {
-  // and put your code here.
+  // Find a number greater than Pi in a double array, print number and its index.
   }
   
   void soln19() {
-  // and put your code here.
+  // Given an array of objects (of class Xaction) w/nulls, sum all
+    // the values of the objects in the array, using Xaction::getValue(). Print the sum.
   }
   
   void soln20() {
-  // and put your code here.
+  // reverse an array of objects of type Xaction
   }
   
   void soln21() {
-  // and put your code here.
+  // Print a 5's times table
   }
   
   void soln22() {
-  // and put your code here.
+  // determine the hypotenuse of a 9 by 7 right triangle, assign it to a double
   }
   
 }
